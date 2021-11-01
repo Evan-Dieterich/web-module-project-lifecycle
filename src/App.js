927fc9b29f3223872c0e511d966e657ce5b1afd5
Evan Dieterich <evan.dieterich.az@gmail.com>
2021-10-31 20:22:04 -0700

    App,js API calls functioning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import axios from 'axios'
+import user from './components/User';
+
+const fetchUser = (user, followers) => {
+  return axios.get(`https://api.github.com/users/${user}${followers}`)
+      .then(res => res)
+      .catch(err => err)
+}
 
 class App extends React.Component {
+  state = {
+    userData : {},
+    user : 'Evan-Dieterich',
+    followers : []
+  };
+
+  componentDidMount() {
+    fetchUser(this.state.user, '')
+      .then(res => {
+        this.setState({
+          ...this.state,
+          userData : res.data
+        })
+        console.log(res.data)
+      })
+     .catch()
+  }
+
   render() {
     return(<div>
-      Github Card
+      <User></User>
     </div>);
   }
 }
